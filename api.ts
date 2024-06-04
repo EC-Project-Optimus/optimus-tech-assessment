6762096dff6f916fb8b24fdb4e6211ae493e89f9
tmak-ec <118852252+tmak-ec@users.noreply.github.com>
2024-06-04 15:11:53 -0400

    Update api.ts

--- api.ts
+++ api.ts
@@ -13,20 +13,20 @@ export class User {
   id: number
 
   @Column()
-  name: string
+  email: string
 }
 
 @Entity()
-export class UserTag {
+export class Vote {
   @Column()
   userId: number
   
   @Column()
-  tagId: number
+  colorId: number
 }
 
 @Entity()
-export class Tag {
+export class Color {
   @PrimaryGeneratedColumn()
   id: number
   
@@ -35,42 +35,48 @@ export class Tag {
 }
 
 
-app.get('/users', async (req: Request, res: Response) => {
-  const skip = parseInt(req.query.skip)
-  const take = parseInt(req.query.take)
+app.get('/poll-result', async (req: Request, res: Response) => {  
+  const votes = await db.getRepository(Vote).find()
   
-  // Let's say the underlying database does not support JOIN
-  const users = await db.getRepository(User).find()
-    .skip(skip)
-    .take(take)
-  
-  const results = []
-  for (const user of users) {
-    const userTags = await db.getRepository(UserTag)
-      .find({ where: { userId: user.id } })
-    
-    const tags = await db.getRepository(Tag)
-      .find({ where: {id: in(userTags.map(t=>t.id)) } })
-    
-    results.push({user, tags})
+  let results = {}
+  for (const vote of votes) {
+    results[vote.colorId] = (results[vote.colorId] || 0) + 1
   }
-  res.send(results);
+
+  const responseBody = []
+  for (const [colorId, votes] of Object.entries(results)) {
+    const color = await db.getRepository(Color).findById(vote.colorId)
+    respondBody.push({
+      color: color.value,
+      votes
+    })
+  }
+  
+  res.send(responseBody);
 });
 
-interface CreateUserPayload {
-  name: string
-  tags: number[]
+interface PollVoteInput {
+  email: string
+  colors: string[]
 }
 
-app.post('/users',  async (req: Request, res: Response) => {
-  const body = req.body as CreateUserPayload
-  const user = await db.getRepository(User)
-    .create({
-      name: body.name
-    })
-  for (const tag of body.tags) {
-    db.getRepository(UserTag)
-      .create({userId: user.id, tagId: tag})
+app.post('/poll-vote',  async (req: Request, res: Response) => {
+  const body = req.body as PollVoteInput
+  
+  let user = await db.getRepository(User).find({where: {email: body.email }})
+  if (user) {
+    throw new Error('User exist already')
+  }
+  
+  user = await db.getRepository(User).create({email: body.email})
+  
+  for (const vote of body.color) {
+    const color = await db.getRepository(Color).create({email: body.color})
+    if (!color) {
+      throw new Error('Color does not exist')
+    }
+    await db.getRepository(Vote)
+      .create({userId: user.id, colorId: vote})
   }
 })
 
